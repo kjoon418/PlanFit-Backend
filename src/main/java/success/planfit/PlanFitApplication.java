29c9e0d731ae8cc02b2fab90eb0d9c1004bdab87
Joon Kim <kjoon418@naver.com>
2025-01-13 12:14:35 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/success/planfit/PlanFitApplication.java
@@ -0,0 +1,13 @@
+package success.planfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanFitApplication.class, args);
+    }
+
+}
