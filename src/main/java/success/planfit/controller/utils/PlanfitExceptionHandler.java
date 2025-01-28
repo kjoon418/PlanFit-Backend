20ec17138ca883a1d06816e203b12fec1670b452
Joon Kim <kjoon418@naver.com>
2025-01-28 11:42:43 +0900

    feat: 커스텀 예외 추가 및 기존 클래스에 사용

--- src/main/java/success/planfit/controller/utils/PlanfitExceptionHandler.java
+++ src/main/java/success/planfit/controller/utils/PlanfitExceptionHandler.java
@@ -1,9 +1,12 @@
 package success.planfit.controller.utils;
 
+import io.jsonwebtoken.MalformedJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import success.planfit.exception.EntityNotFoundException;
+import success.planfit.exception.IllegalRequestException;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 
@@ -14,14 +17,18 @@ import static org.springframework.http.HttpStatus.*;
 public class PlanfitExceptionHandler {
 
     public ResponseEntity<String> handle(Exception e) {
-        log.info("MyChatExceptionHandler.handle() called");
+        log.info("PlanfitExceptionHandler.handle() called");
 
-        if (e instanceof IllegalArgumentException) {
+        if (e instanceof EntityNotFoundException ||
+                e instanceof IllegalRequestException) {
             return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
         }
         if (e instanceof SQLIntegrityConstraintViolationException ||
                 e instanceof DataIntegrityViolationException) {
             return ResponseEntity.status(BAD_REQUEST).body("다른 사용자가 이미 사용하고 있는 값이거나, 이미 처리된 요청입니다.");
+        }
+        if (e instanceof MalformedJwtException) {
+            return ResponseEntity.status(UNAUTHORIZED).body("부적절한 JWT 토큰입니다.");
         }
 
         log.error("Unhandled error occurred", e);
