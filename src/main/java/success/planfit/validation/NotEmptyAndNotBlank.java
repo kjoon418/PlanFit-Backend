4ee14fb3a4a56b1bf1b8114f00b890b4367fd745
Joon Kim <kjoon418@naver.com>
2025-02-01 21:20:43 +0900

    feat: 파라미터 검증을 위한 커스텀 어노태이션 생성

---
+++ src/main/java/success/planfit/validation/NotEmptyAndNotBlank.java
@@ -0,0 +1,23 @@
+package success.planfit.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = NotEmptyAndNotBlankValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotEmptyAndNotBlank {
+
+    String value();
+
+    boolean allowWhiteSpace() default true;
+
+    String message() default "잘못된 요청입니다.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
