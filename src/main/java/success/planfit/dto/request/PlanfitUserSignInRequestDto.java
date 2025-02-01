c4fa9a385c42f5e4f0a61db0dcb05c820b9a2598
Joon Kim <kjoon418@naver.com>
2025-02-01 21:22:59 +0900

    chore: RequestDto의 필수 파라미터들에 @NotEmptyAndNotBlank 추가

--- src/main/java/success/planfit/dto/request/PlanfitUserSignInRequestDto.java
+++ src/main/java/success/planfit/dto/request/PlanfitUserSignInRequestDto.java
@@ -1,10 +1,15 @@
 package success.planfit.dto.request;
 
 import lombok.Getter;
+import success.planfit.validation.NotEmptyAndNotBlank;
 
 @Getter
 public class PlanfitUserSignInRequestDto {
 
+    @NotEmptyAndNotBlank(value = "loginId")
     private String loginId;
+
+    @NotEmptyAndNotBlank(value = "password")
     private String password;
+
 }
--- src/main/java/success/planfit/dto/request/SpaceBookmarkRegistrationRequestDto.java
+++ src/main/java/success/planfit/dto/request/SpaceBookmarkRegistrationRequestDto.java
@@ -5,17 +5,33 @@ import success.planfit.domain.bookmark.SpaceBookmark;
 import success.planfit.domain.course.SpaceType;
 import success.planfit.domain.embeddable.SpaceInformation;
 import success.planfit.photo.PhotoProvider;
+import success.planfit.validation.NotEmptyAndNotBlank;
 
 @Getter
 public class SpaceBookmarkRegistrationRequestDto {
 
+    @NotEmptyAndNotBlank(value = "googlePlacesIdentifier")
     private String googlePlacesIdentifier;
+
+    @NotEmptyAndNotBlank(value = "spaceName")
     private String spaceName;
+
+    @NotEmptyAndNotBlank(value = "location")
     private String location;
+
+    @NotEmptyAndNotBlank(value = "spaceType")
     private SpaceType spaceType;
+
+    @NotEmptyAndNotBlank(value = "link")
     private String link;
+
+    @NotEmptyAndNotBlank(value = "latitude")
     private Double latitude;
+
+    @NotEmptyAndNotBlank(value = "longitude")
     private Double longitude;
+
+    @NotEmptyAndNotBlank(value = "spacePhoto")
     private String spacePhoto; // 우선 Base64로 인코딩된 사진 정보를 받아온다고 가정함
 
     public SpaceBookmark toEntity() {
