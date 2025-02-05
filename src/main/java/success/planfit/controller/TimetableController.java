0b05ee1d4f0b5f019102f64e5883eed661e59c2e
Chae, KyungWan <house05240@naver.com>
2025-02-05 17:58:03 +0900

    refactor: 시간표 api url 수정

--- src/main/java/success/planfit/controller/TimetableController.java
+++ src/main/java/success/planfit/controller/TimetableController.java
@@ -24,7 +24,7 @@ public class TimetableController {
 
 
     // 날짜를 기준으로 시간표 등록
-    @PostMapping("/add/{date}")
+    @PostMapping("/{date}")
     public ResponseEntity<Void> addTimetable(@PathVariable LocalDate date, @RequestBody TimetableCreationRequestDto requestDto, Principal principal) {
         Long userId = controllerUtil.findUserIdByPrincipal(principal);
         timetableService.addTimetable(userId,requestDto,date);
@@ -32,7 +32,7 @@ public class TimetableController {
     }
 
     // 날짜를 기준으로 시간표 삭제
-    @DeleteMapping("/remove/{date}/{timetableId}")
+    @DeleteMapping("/{date}/{timetableId}")
     public ResponseEntity<String> removeTimetable(@PathVariable LocalDate date, Principal principal, @PathVariable Long timetableId) {
         Long userId = controllerUtil.findUserIdByPrincipal(principal);
         timetableService.removeTimetable(userId, date ,timetableId);
@@ -40,14 +40,14 @@ public class TimetableController {
     }
 
     // 날짜를 기준으로 시간표 수정
-    @PatchMapping("/update/{date}/{timetableId}")
+    @PatchMapping("/{date}/{timetableId}")
     public ResponseEntity<TimetableInfoResponseDto> updateTimetable(@PathVariable LocalDate date, @PathVariable Long timetableId, Principal principal, @RequestBody TimetableUpdateRequestDto updateRequestDto ) {
         Long userId = controllerUtil.findUserIdByPrincipal(principal);
         return ResponseEntity.ok(timetableService.updateTimetable(userId, date, timetableId,updateRequestDto));
     }
 
     // 특정 날짜의 시간표 조회
-    @GetMapping("/get/{date}")
+    @GetMapping("/{date}")
     public ResponseEntity<List<TimetableInfoResponseDto>> getTimetables(@PathVariable LocalDate date, Principal principal) {
         Long userId = controllerUtil.findUserIdByPrincipal(principal);
         return ResponseEntity.ok(timetableService.getTimetables(userId, date));
