581abc6fae1741b069d118ba941778e7be61ee05
Chae, KyungWan <house05240@naver.com>
2025-03-16 22:17:58 +0900

    feat: 포스트 좋아요, 포스트 좋아요 조회, 포스트 좋아요 취소

---
+++ src/main/java/success/planfit/post/controller/PostLikeController.java
@@ -0,0 +1,35 @@
+package success.planfit.post.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import success.planfit.post.service.PostLikeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/likes")
+@RequiredArgsConstructor
+public class PostLikeController {
+
+    private final PostLikeService postLikeService;
+
+    @PostMapping("/{postId}")
+    public ResponseEntity<String> likePost(@RequestParam Long userId, @PathVariable Long postId) {
+        postLikeService.likePost(userId, postId);
+
+        return ResponseEntity.ok("포스트를 좋아요 했습니다.");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Long>> getLikedPosts(@RequestParam Long userId) {
+        return ResponseEntity.ok(postLikeService.getLikedPosts(userId));
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<String> unlikePost(@RequestParam Long userId, @PathVariable Long postId) {
+        postLikeService.unlikePost(userId, postId);
+
+        return ResponseEntity.ok("포스트 좋아요 취소했습니다.");
+    }
+}
