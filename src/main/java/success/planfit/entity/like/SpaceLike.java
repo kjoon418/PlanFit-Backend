2d1cf7667c59ad40799fea02d5301e6885c222e2
Chae, KyungWan <house05240@naver.com>
2025-03-24 23:02:57 +0900

    refactor: Space -> SpaceDetail
    ManyToOne을 Space -> SpaceDetail
    builder 파라미터 변경 Space -> SpaceDetail

--- src/main/java/success/planfit/entity/like/SpaceLike.java
+++ src/main/java/success/planfit/entity/like/SpaceLike.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import success.planfit.entity.space.Space;
+import success.planfit.entity.space.SpaceDetail;
 import success.planfit.entity.user.User;
 
 import static lombok.AccessLevel.PROTECTED;
@@ -19,15 +19,15 @@ public class SpaceLike {
 
     @JoinColumn(nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
-    private Space space;
+    private SpaceDetail spaceDetail;
 
     @JoinColumn(nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
     @Builder
-    private SpaceLike(Space space, User user){
-        this.space = space;
+    private SpaceLike(SpaceDetail spaceDetail, User user){
+        this.spaceDetail = spaceDetail;
         this.user = user;
     }
 
