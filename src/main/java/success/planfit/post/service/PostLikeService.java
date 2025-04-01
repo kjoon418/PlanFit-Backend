72c86c80b04bd727d6ba2265deee8fb75cb464dc
Chae, KyungWan <house05240@naver.com>
2025-04-01 17:23:05 +0900

    refactor: postlike로 buid
    postLikeRepository에서 저장하던걸 postlike에서 빌드

--- src/main/java/success/planfit/post/service/PostLikeService.java
+++ src/main/java/success/planfit/post/service/PostLikeService.java
@@ -6,10 +6,10 @@ import org.springframework.transaction.annotation.Transactional;
 import success.planfit.entity.like.PostLike;
 import success.planfit.entity.post.Post;
 import success.planfit.entity.user.User;
+import success.planfit.global.exception.EntityNotFoundException;
 import success.planfit.repository.PostLikeRepository;
 import success.planfit.repository.PostRepository;
 import success.planfit.repository.UserRepository;
-import success.planfit.user.service.UserService;
 
 import java.util.List;
 
@@ -34,10 +34,12 @@ public class PostLikeService {
                     throw new IllegalArgumentException("이미 좋아요한 포스트입니다.");
                 });
 
-        postLikeRepository.save(PostLike.builder()
+        PostLike postlike = PostLike.builder()
                 .user(user)
                 .post(post)
-                .build());
+                .build();
+
+        user.addPostLike(postlike);
         post.increaseLikeCount();
     }
 
@@ -50,12 +52,15 @@ public class PostLikeService {
     }
 
     public void unlikePost(Long postId, Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("유저를 찾을 수 없습니다."));
+
         PostLike postLike = postLikeRepository.findByUserIdAndPostId(userId, postId)
                 .orElseThrow(() -> new IllegalArgumentException("좋아요하지 않은 포스트입니다."));
 
-        postLikeRepository.delete(postLike);
+        user.removePostLike(postLike);
         postLike.getPost().decreaseLikeCount();
     }
 
 }
-
