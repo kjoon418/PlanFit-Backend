75df709a7375d0cd43ab2315ab0ab7f091d72c81
Kim Joon <meaninglessw@gamil.com>
2025-04-07 11:55:34 +0900

    refactor: 코드 가독성 개선

--- src/main/java/success/planfit/user/service/UserService.java
+++ src/main/java/success/planfit/user/service/UserService.java
@@ -21,7 +21,6 @@ import java.util.function.Supplier;
 public class UserService {
 
     private static final Supplier<EntityNotFoundException> USER_NOT_FOUND_EXCEPTION = () -> new EntityNotFoundException("해당 ID로 회원을 조회할 수 없습니다.");
-    private static final Supplier<EntityNotFoundException> USER_NOT_FOUND_EXCEPTION_BY_TOKEN = () -> new EntityNotFoundException("해당 JWT 토큰으로 회원을 조회할 수 없습니다.");
 
     private final UserRepository userRepository;
     private final TokenProvider tokenProvider;
@@ -77,9 +76,8 @@ public class UserService {
 
     @Transactional(readOnly = true)
     public AccessTokenResponseDto reissueAccessToken(String refreshToken) {
-        long userId = Long.parseLong(tokenProvider.parseClaims(refreshToken).getSubject());
-        User user = userRepository.findById(userId)
-                .orElseThrow(USER_NOT_FOUND_EXCEPTION_BY_TOKEN);
+        long userId = findUserIdFromToken(refreshToken);
+        User user = findUserById(userId);
 
         if (!tokenProvider.validateToken(refreshToken, TokenType.REFRESH)) {
             throw new IllegalRequestException("부적절한 리프레쉬 토큰입니다.");
@@ -97,6 +95,13 @@ public class UserService {
     private User findUserById(long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(USER_NOT_FOUND_EXCEPTION);
+    }
+
+    private long findUserIdFromToken(String token) {
+        String userId = tokenProvider.parseClaims(token)
+                .getSubject();
+
+        return Long.parseLong(userId);
     }
 
     private boolean isEqualWithSavedToken(User user, String refreshTokenValue) {
